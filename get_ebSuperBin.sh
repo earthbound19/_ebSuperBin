54950f6fdc4288eb143112b5479d5967bc4fe1d0
Alex Hall <earthbound19@users.noreply.github.com>
2022-12-28 21:07:29 -0700

    add uknown source :| binaries for Poppler utils

--- get_ebSuperBin.sh
+++ get_ebSuperBin.sh
@@ -6,7 +6,7 @@ echo "Downloading _ebSuperBin.7z . . ."
 wget https://earthbound.io/data/dist/_ebSuperBin.7z
 
 sha256arr=( $(sha256sum ./_ebSuperBin.7z) )
-checksum='8a170fcee02a00864cdd4d84acd9a1560f8dc44b301af7c71631ea57501430fb'
+checksum='0fad49c16a4952c740e3f53244ad30323c428117006d3894f1f6e92174b1b485'
 
 echo "Checking sha256 sum of downloaded archive for match of:"
 echo "$checksum"
