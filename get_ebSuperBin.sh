836c713e48a6ed73fec05501803b661184f1d623
Alex Hall <earthbound19@users.noreply.github.com>
2023-05-11 11:11:29 -0600

    per new es.exe in _ebSuperBin.7z

--- get_ebSuperBin.sh
+++ get_ebSuperBin.sh
@@ -25,7 +25,7 @@ echo "Downloading _ebSuperBin.7z . . ."
 wget https://earthbound.io/data/dist/_ebSuperBin.7z
 
 sha256arr=( $(sha256sum ./_ebSuperBin.7z) )
-checksum='c80cf3314cfc957ae46202aec860089c68ae4a2ed25d8d92ea5ea4be27cc4592'
+checksum='d693dc81f7178f2b239a3b64b47a0721fe83cf049a3bdc7ce6168ff7724ed842'
 
 echo "Checking sha256 sum of downloaded archive for match of:"
 echo "$checksum"
